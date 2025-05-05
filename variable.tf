eb124d11296056a7c6f16fa0d45db08f5a2c135e
Sujeet Rahate <sujeetsrahate@gmail.com>
2025-05-05 15:25:59 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,10 +1,9 @@
-# terraform-modules/ec2-instance/variables.tf
-variable "ami_id" {
-  description = "The AMI ID to use for the instance"
-  type        = string
-}
+variable "aws_ami_id" {
+    description = "The AMI ID to use for the instance"
+    type = string
+    }
 
 variable "instance_type" {
-  description = "The type of the instance"
-  type        = string
+    description = "The type of the instance"
+    type = string
 }
