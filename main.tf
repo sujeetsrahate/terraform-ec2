50712ba0c5d11b0671e2ac4b4a5bad92c3f428ce
Sujeet Rahate <sujeetsrahate@gmail.com>
2025-05-05 15:25:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
-# terraform-modules/ec2-instance/main.tf
-resource "aws_instance" "example" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+resource "aws_instance" "web" {
+
+ ami = var.aws_ami_id
+
+ instance_type = var.instance_type
+
+ tags = {
+
+  Name = "Example2-Instance"
+
+ }
+
 }
